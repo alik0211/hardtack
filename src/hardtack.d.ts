b0a86426ad85cd5e378d3f33c40cf6f0e8ec84a0
Ali Gasymov <alik0211alik@gmail.com>
2018-11-19 11:45:01 +0400

    Add declaration file on TypeScript

---
+++ src/hardtack.d.ts
@@ -0,0 +1,18 @@
+export as namespace hardtack;
+
+interface Options {
+  path?: string,
+  domain?: string,
+  'max-age'?: number,
+  expires?: string,
+  secure?: boolean,
+  samesite?: 'lax' | 'strict',
+}
+
+export function set(name: string, value: string, options: Options): string;
+
+export function get(): object | undefined;
+
+export function get(name: string): string | undefined;
+
+export function remove(name: string, options: Options): string;
